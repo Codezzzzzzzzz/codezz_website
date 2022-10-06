e5f671db693b9c751364fb9848fb2f37cd411a12
Vikram Srinivas <vikramksm@hotmail.com>
2022-10-06 14:13:30 +0000

    vite init

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import './App.css'
+import 'animate.css'
+function App() {
+
+
+  return (
+    <div>
+
+
+    <div className="animate__animated animate__backInDown">  
+    <img src="https://avatars.githubusercontent.com/u/103253731?s=400&u=76a8bfe54ec383ff70a60aa3e67bccabdeaffb94&v=4" className="logo" alt="Codezz logo" />
+      <h1>
+
+      </h1>
+    </div>
+
+    <div>
+      
+    </div>
+    </div>
+  )
+}
+
+export default App
